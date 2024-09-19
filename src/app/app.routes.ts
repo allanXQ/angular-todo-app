2a9deadda685b23278173dec06df8aa41ef66a2a
allanXQ <allancjuma@gmail.com>
2024-09-19 10:48:08 +0300

    add edit route, components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
 import { CreateTodoComponent } from './create-todo/create-todo.component';
 import { TodoComponent } from './todo/todo.component';
+import { EditTodoComponent } from './edit-todo/edit-todo.component';
 
 export const routes: Routes = [
   { path: 'todos', component: TodosComponent },
   {
     path: 'todo/:todoId',
     component: TodoComponent,
+  },
+  {
+    path: 'edit/:todoId',
+    component: EditTodoComponent,
   },
   {
     path: 'create-todo',
