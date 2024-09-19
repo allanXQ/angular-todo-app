b45bc93a9df91cbee65e639b9e6aec6a1971b2e8
allanXQ <allancjuma@gmail.com>
2024-09-19 10:21:50 +0300

    add todo table

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -13,17 +13,39 @@ export class TodosComponent {
     {
       id: 1,
       title: 'Learn Angular 2',
+      description: 'Learn Angular 2 by building a simple app',
       completed: false,
     },
     {
       id: 2,
       title: 'Learn Angular 2 Router',
+      description: 'Learn Angular 2 Router by building a simple app',
       completed: false,
     },
     {
       id: 3,
       title: 'Learn Angular 2 Forms',
+      description: 'Learn Angular 2 Forms by building a simple app',
+      completed: false,
+    },
+    {
+      id: 4,
+      title: 'Learn Angular 2 Services',
+      description: 'Learn Angular 2 Services by building a simple app',
+      completed: false,
+    },
+    {
+      id: 5,
+      title: 'Learn Angular 2 Pipes',
+      description: 'Learn Angular 2 Pipes by building a simple app',
       completed: false,
     },
   ];
+
+  editTodo = (todo: number) => {
+    console.log('Edit Todo:', todo);
+  };
+  deleteTodo = (todo: number) => {
+    console.log('Delete Todo:', todo);
+  };
 }
