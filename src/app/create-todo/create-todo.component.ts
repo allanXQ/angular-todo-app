4249fd700b18266dd698c699fbc745a006161def
allanXQ <allancjuma@gmail.com>
2024-09-19 12:29:28 +0300

    add create todo form

--- src/app/create-todo/create-todo.component.ts
+++ src/app/create-todo/create-todo.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
 @Component({
   selector: 'app-create-todo',
   standalone: true,
   imports: [],
   templateUrl: './create-todo.component.html',
-  styleUrl: './create-todo.component.css'
+  styleUrl: './create-todo.component.css',
 })
 export class CreateTodoComponent {
-
+  todo: Todo = {} as Todo;
+  createTodo = (todo: Todo) => {
+    console.log(todo);
+  };
 }
