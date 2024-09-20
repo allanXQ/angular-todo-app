6ab67cfbfba8aa72e87e261da68dd40b29219bfa
allanXQ <allancjuma@gmail.com>
2024-09-20 10:45:30 +0300

    fetch totod by param todoId

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,12 +1,38 @@
-import { Component, input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TodoService } from '../todo.service';
+import { Todo } from '../interfaces';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
   templateUrl: './todo.component.html',
-  styleUrl: './todo.component.css',
+  styleUrls: ['./todo.component.css'],
 })
-export class TodoComponent {
-  todoId = input.required<string>();
+export class TodoComponent implements OnInit {
+  todoId!: number;
+  todo!: Todo;
+
+  constructor(
+    private todoService: TodoService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      const idParam = params.get('todoId');
+      this.todoId = idParam ? Number(idParam) : 0;
+
+      if (this.todoId !== 0) {
+        const todo = this.todoService.getTodoById(this.todoId);
+        if (todo) {
+          this.todo = todo;
+        } else {
+          console.error(`Todo with id ${this.todoId} not found.`);
+        }
+      } else {
+        console.error('Invalid or missing todo ID in route parameters.');
+      }
+    });
+  }
 }
